7e28a775aaf159a83b93990e8fdc31cea413aff6
kevin <kevindu032270@gmail.com>
2023-03-17 13:22:45 +0100

    remove move classe.py into character.py

---
+++ characters/character.py
@@ -0,0 +1,12 @@
+class Character:
+    def __init__(self, name, HP, defense, helmet, chestplate, leggings, boots, weapon1, weapon2):
+        self.name = name
+        self.HP = HP
+        self.defense = defense
+        
+        self.helmet = helmet
+        self.chestplate = chestplate
+        self.leggings = leggings
+        self.boots = boots
+        self.weapon1 = weapon1
+        self.weapon2 = weapon2
