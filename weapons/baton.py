0b7dcf3fe26f0f4fe484a9db136ae658ee2bb62b
kevin <kevindu032270@gmail.com>
2023-03-30 18:37:05 +0200

    commentaire du code

--- weapons/baton.py
+++ weapons/baton.py
@@ -1,15 +1,15 @@
 class Baton:
-    def __init__(self, name:str, damage:int):
+    def __init__(self, name:str, damage:int):  # constructeur avec deux attributs : nom et dégâts
         self.name = name
         self.damage = damage
     
-    def get_dmg(self) -> int:
+    def get_dmg(self) -> int:  # méthode qui retourne les dégâts du bâton
         return self.damage
     
-    def __str__(self) -> str:
-        text = self.name +":"
-        if self.damage:
-            text += f"\r\n  {self.damage} damage"
+    def __str__(self) -> str:  # méthode qui retourne une chaîne de caractères décrivant le bâton
+        text = self.name +":"  # ajoute le nom du bâton
+        if self.damage:  # si les dégâts sont non nuls
+            text += f"\r\n  {self.damage} damage"  # ajoute la description des dégâts
         return text
     
-    noms_batons = ['Le bâton du chaos', 'Le bâton qui démange', 'Le bâton de la cacophonie', 'Le bâton de la licorne']
+    noms_batons = ['Le bâton du chaos', 'Le bâton qui démange', 'Le bâton de la cacophonie', 'Le bâton de la licorne']  # liste de noms de bâtons prédéfinis
--- weapons/masse.py
+++ weapons/masse.py
@@ -1,15 +1,19 @@
 class Masse:
     def __init__(self, name:str, damage:int):
+        # Initialise les attributs "name" et "damage" de la classe Masse
         self.name = name
         self.damage = damage
     
     def get_dmg(self) -> int:
+        # Retourne la valeur de l'attribut "damage"
         return self.damage
     
     def __str__(self) -> str:
+        # Retourne une chaîne de caractères représentant la masse, y compris son nom et les dégâts infligés
         text = self.name +":"
         if self.damage:
             text += f"\r\n  {self.damage} damage"
         return text
     
     noms_de_masse = ["Le Fléau du Fromage Fondant", "La Foudre Frite", "L'Ouragan des Olives", "Le Tremblement de Terre aux Tomates"]
+    # Attribut de classe contenant une liste de noms de masses disponibles
